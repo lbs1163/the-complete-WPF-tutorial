abe77662dc44808da70fe39896b7d9c5aa1a9ba4
Beomsu Lim <lbs1163@postech.ac.kr>
2019-12-04 13:39:33 +0900

    Events in XAML

--- the-complete-WPF-tutorial/MainWindow.xaml.cs
+++ the-complete-WPF-tutorial/MainWindow.xaml.cs
@@ -23,6 +23,12 @@ namespace the_complete_WPF_tutorial
         public MainWindow()
         {
             InitializeComponent();
+            pnlMainGrid.MouseUp += new MouseButtonEventHandler(pnlMainGrid_MouseUp);
+        }
+
+        private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
         }
     }
 }
