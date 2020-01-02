eb1e8e5b3750e97cf4c08c43b482f75b445ba174
Beomsu Lim <lbs1163@postech.ac.kr>
2020-01-02 15:54:12 +0900

    The Button control

--- the-complete-WPF-tutorial/MainWindow.xaml.cs
+++ the-complete-WPF-tutorial/MainWindow.xaml.cs
@@ -11,12 +11,9 @@ namespace the_complete_WPF_tutorial
             InitializeComponent();
         }
 
-        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        private void HelloWorldButton_Click(object sender, RoutedEventArgs e)
         {
-            TextBox textBox = sender as TextBox;
-            txtStatus.Text = "Selection starts at character #" + textBox.SelectionStart + Environment.NewLine;
-            txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
-            txtStatus.Text += "Selected text: '" + textBox.SelectedText + "'";
+            MessageBox.Show("Hello, world!");
         }
     }
 }
