43d8512b7d9ddad46ba6e5d1f34a87c4ecc8f829
Beomsu Lim <lbs1163@postech.ac.kr>
2020-01-02 15:35:28 +0900

    The TextBox control

--- the-complete-WPF-tutorial/MainWindow.xaml.cs
+++ the-complete-WPF-tutorial/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace the_complete_WPF_tutorial
 {
@@ -8,6 +9,14 @@ namespace the_complete_WPF_tutorial
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            txtStatus.Text = "Selection starts at character #" + textBox.SelectionStart + Environment.NewLine;
+            txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
+            txtStatus.Text += "Selected text: '" + textBox.SelectedText + "'";
         }
     }
 }
