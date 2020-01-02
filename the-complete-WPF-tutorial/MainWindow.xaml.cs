5533e43beac7b03a57d0563f13470f94e0e86a1e
Beomsu Lim <lbs1163@postech.ac.kr>
2020-01-02 20:36:55 +0900

    The Image control

--- the-complete-WPF-tutorial/MainWindow.xaml.cs
+++ the-complete-WPF-tutorial/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
 
 namespace the_complete_WPF_tutorial
 {
@@ -9,6 +10,22 @@ namespace the_complete_WPF_tutorial
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnLoadFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                imgDynamic.Source = new BitmapImage(fileUri);
+            }
+        }
+
+        private void BtnLoadFromResource_Click(object sender, RoutedEventArgs e)
+        {
+            Uri resourceUri = new Uri("/Images/white_bengal_tiger.jpg", UriKind.Relative);
+            imgDynamic.Source = new BitmapImage(resourceUri);
         }
     }
 }
