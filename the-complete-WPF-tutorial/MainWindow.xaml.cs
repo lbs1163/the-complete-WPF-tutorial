7690dc41005025d71e15230d89fab95e6091fa09
Beomsu Lim <lbs1163@postech.ac.kr>
2020-01-06 15:20:51 +0900

    Control ToolTips

--- the-complete-WPF-tutorial/MainWindow.xaml.cs
+++ the-complete-WPF-tutorial/MainWindow.xaml.cs
@@ -11,21 +11,5 @@ namespace the_complete_WPF_tutorial
         {
             InitializeComponent();
         }
-
-        private void BtnLoadFromFile_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-            {
-                Uri fileUri = new Uri(openFileDialog.FileName);
-                imgDynamic.Source = new BitmapImage(fileUri);
-            }
-        }
-
-        private void BtnLoadFromResource_Click(object sender, RoutedEventArgs e)
-        {
-            Uri resourceUri = new Uri("/Images/white_bengal_tiger.jpg", UriKind.Relative);
-            imgDynamic.Source = new BitmapImage(resourceUri);
-        }
     }
 }
